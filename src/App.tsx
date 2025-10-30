a6e4a8d4ecf2cbea2a5dcc63588a03b79d5735ed
Pedro Antônio <pedroantonioytbr@gmail.com>
2025-10-30 10:40:04 -0300

    feat: add controlled input with typed event (Day 3)
    Added a controlled input component using React + TypeScript with a properly typed onChange event.
    Used React.ChangeEvent<HTMLInputElement> to ensure strong typing and prevent any usage

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,22 @@ function App() {
     setValor(valor - 1)
   }
 
+  const [text, setText] = useState<string>("")
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setText(event.target.value)
+  }
+
   return (
     <main className="App">
       <h1>Praticando React com Typescript</h1>
       <Count valor={valor} />
       <button onClick={handleSum}>Somar 1</button>
       <button onClick={handleSub}>Subtrair 1</button>
+      <div className='display'>
+        {text}
+      </div>
+      <input type="text" placeholder='Digite Aqui' onChange={handleChange} value={text} />
     </main>
   )
 }
